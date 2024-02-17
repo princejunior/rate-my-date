bd02fdab359f8743f8ec34f70e919a502ff5b51a
Elijah Elliott <elliott.t.elijah@gmail.com>
2024-02-17 01:17:11 -0700

    added more template files

--- rmd/rmd_web/views.py
+++ rmd/rmd_web/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from fire.fireconfig import Firebase
 
 # Create your views here.
+
+def home(request):
+    # initFire = Firebase()
+    # initFire.get_data()
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
+
+def comment():
+    return
+
+def createNewPerson(request):
+    template = loader.get_template('createnewperson.html')
+    return HttpResponse(template.render())
